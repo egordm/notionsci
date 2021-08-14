8581035ac721d5ddb1844ff02b5b4a72762ec432
Egor Dmitriev <egordmitriev2@gmail.com>
2021-08-14 16:38:39 +0200

    [FEATURE] Added config migrations [REFACTOR] Split config.py into a module

---
+++ notionsci/config/constants.py
@@ -0,0 +1,6 @@
+CONFIG_VERSION = 1
+APP_NAME = "notionsci"
+DEFAULT_PROFLE = "default"
+OVERRIDE_CONFIG_NAME = "config.yml"
+
+TEMPLATE_ZOTERO = 'https://efficacious-alarm-7cc.notion.site/Zotero-Library-dd4b26a3b11d46518b70b5031aee8989'
